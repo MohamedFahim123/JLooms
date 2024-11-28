c4180fd97a22a7ee2975f44f0d2fd6684048a7f4
mohamedfahim1045 <mohamedfahim1045@gmail.com>
2024-11-28 14:04:48 +0200

    update and solve an error in deploy 6

---
+++ app/dashboard/utils/tableData.ts
@@ -0,0 +1,16 @@
+import { Table } from "./interfaces";
+import teacher from '../../imgs/teachers/teacher1.png';
+
+export const students: Table[] = [
+    { id: 1, classDetails: '', address: '1th elhoria streat ,zefta ,egypt', email: 'student@email.com', phone: '0123 456 789', age: 4, gender: 'male', image: teacher.src, name: 'Kristin Watson', subject: 'Multiple Subjects', class: 'Multiple Classes', parents: 'Mother and Father' },
+    { id: 2, classDetails: '', address: '1th elhoria streat ,zefta ,egypt', email: 'student@email.com', phone: '0123 456 789', age: 4, gender: 'female', image: teacher.src, name: 'Marvin McKinney', subject: 'French', class: 'JSS 3', parents: 'Mother and Father' },
+    { id: 3, classDetails: '', address: '1th elhoria streat ,zefta ,egypt', email: 'student@email.com', phone: '0123 456 789', age: 3, gender: 'male', image: teacher.src, name: 'Jane Cooper', subject: 'Maths', class: 'JSS 3', parents: 'Mother and Father' },
+    { id: 4, classDetails: '', address: '1th elhoria streat ,zefta ,egypt', email: 'student@email.com', phone: '0123 456 789', age: 3, gender: 'female', image: teacher.src, name: 'Cody Fisher', subject: 'Multiple Subjects', class: 'SS 3', parents: 'Mother and Father' },
+    { id: 5, classDetails: '', address: '1th elhoria streat ,zefta ,egypt', email: 'student@email.com', phone: '0123 456 789', age: 4, gender: 'male', image: teacher.src, name: 'Bessie Cooper', subject: 'Social Studies', class: 'Multiple Classes', parents: 'Mother and Father' },
+    { id: 6, classDetails: '', address: '1th elhoria streat ,zefta ,egypt', email: 'student@email.com', phone: '0123 456 789', age: 4, gender: 'female', image: teacher.src, name: 'Leslie Alexander', subject: 'Home Economics', class: 'SS 3', parents: 'Mother and Father' },
+    { id: 7, classDetails: '', address: '1th elhoria streat ,zefta ,egypt', email: 'student@email.com', phone: '0123 456 789', age: 3, gender: 'male', image: teacher.src, name: 'Guy Hawkins', subject: 'Multiple Subjects', class: 'JSS 1', parents: 'Mother and Father' },
+    { id: 8, classDetails: '', address: '1th elhoria streat ,zefta ,egypt', email: 'student@email.com', phone: '0123 456 789', age: 3, gender: 'female', image: teacher.src, name: 'Theresa Webb', subject: 'Psychology', class: 'JSS 3', parents: 'Mother and Father' },
+    { id: 9, classDetails: '', address: '1th elhoria streat ,zefta ,egypt', email: 'student@email.com', phone: '0123 456 789', age: 4, gender: 'male', image: teacher.src, name: 'Jerome Bell', subject: 'Physics', class: 'JSS 4', parents: 'Mother and Father' },
+    { id: 10, classDetails: '', address: '1th elhoria streat ,zefta ,egypt', email: 'student@email.com', phone: '0123 456 789', age: 4, gender: 'female', image: teacher.src, name: 'Savannah Nguyen', subject: 'Accounting', class: 'JSS 4', parents: 'Mother and Father' },
+    { id: 11, classDetails: '', address: '1th elhoria streat ,zefta ,egypt', email: 'student@email.com', phone: '0123 456 789', age: 3, gender: 'male', image: teacher.src, name: 'Wade Warren', subject: 'C.R.S', class: 'JSS 5', parents: 'Mother and Father' },
+];
