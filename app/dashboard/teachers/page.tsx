90a02425093c6290ef1b38aab05d45484d4e6c0f
mohamedfahim1045 <mohamedfahim1045@gmail.com>
2024-12-05 14:08:07 +0200

    solve deploy Error 3

--- app/dashboard/teachers/page.tsx
+++ app/dashboard/teachers/page.tsx
@@ -1,12 +1,8 @@
 import TeachersSection from "@/app/components/TeachersSection/TeachersSection";
 import { Metadata } from "next";
-
-
 export const metadata: Metadata = {
     title: "Our Teachers",
 };
-
-
 
 export default async function TeachersPage() {
     return (
