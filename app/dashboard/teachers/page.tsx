f5381a4cd3dbbb773e1329c1b95109b777b7e081
mohamedfahim1045 <mohamedfahim1045@gmail.com>
2024-12-05 14:17:01 +0200

    solve deploy Error 5

--- app/dashboard/teachers/page.tsx
+++ app/dashboard/teachers/page.tsx
@@ -1,11 +1,14 @@
 import TeachersSection from "@/app/components/TeachersSection/TeachersSection";
 import { Metadata } from "next";
+import { Suspense } from "react";
 export const metadata: Metadata = {
     title: "Our Teachers",
 };
 
 export default function TeachersPage() {
     return (
-        <TeachersSection />
+        <Suspense fallback={<div>Loading...</div>}>
+            <TeachersSection />
+        </Suspense>
     );
 };
