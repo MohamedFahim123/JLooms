2708ad620acb1b9302320379ddcb7498c702242b
mohamedfahim1045 <mohamedfahim1045@gmail.com>
2024-12-11 15:49:06 +0200

    Solve deploy Error 2

--- app/dashboard/utils/tableData.ts
+++ app/dashboard/utils/tableData.ts
@@ -1,6 +1,20 @@
 import { Table } from "./interfaces";
 import teacher from '../../imgs/teachers/teacher1.png';
 
+export const classes: Table[] = [
+    { id: 1, name: 'KG-1', number_of_students: 20, floor: 1},
+    { id: 2, name: 'KG-2', number_of_students: 20, floor: 1},
+    { id: 3, name: 'Class-1', number_of_students: 20, floor: 2},
+    { id: 4, name: 'Class-2', number_of_students: 20, floor: 2},
+    { id: 5, name: 'Class-3', number_of_students: 20, floor: 2},
+    { id: 6, name: 'Class-4', number_of_students: 20, floor: 3},
+    { id: 7, name: 'Class-5', number_of_students: 20, floor: 3},
+    { id: 8, name: 'Class-6', number_of_students: 20, floor: 3},
+    { id: 9, name: 'Primary-1', number_of_students: 20, floor: 4},
+    { id: 10, name: 'Primary-2', number_of_students: 20, floor: 4},
+    { id: 11, name: 'Primary-3', number_of_students: 20, floor: 4},
+];
+
 export const students: Table[] = [
     { id: 1, classDetails: '', address: '1th elhoria streat ,zefta ,egypt', email: 'student@email.com', phone: '0123 456 789', age: 4, gender: 'male', image: teacher.src, name: 'Kristin Watson', subject: 'Multiple Subjects', class: 'Multiple Classes', parents: 'Mother and Father' },
     { id: 2, classDetails: '', address: '1th elhoria streat ,zefta ,egypt', email: 'student@email.com', phone: '0123 456 789', age: 4, gender: 'female', image: teacher.src, name: 'Marvin McKinney', subject: 'French', class: 'JSS 3', parents: 'Mother and Father' },
